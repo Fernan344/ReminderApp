67e0e5d27c6351c092d706914ad1f6a95d161664
fernan344 <fernandoguerra344@gmail.com>
2022-07-23 11:40:58 -0600

    Create Executable

---
+++ ReminderApp/ReminderApp/Executor/Executor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ReminderApp.Executor
+{
+    public class Executor
+    {
+        public static void ExecuteCommand(string _Command)
+        {
+            ProcessStartInfo procStartInfo = new ProcessStartInfo("cmd", "/c " + _Command);
+            // Indicamos que la salida del proceso se redireccione en un Stream
+            procStartInfo.RedirectStandardOutput = true;
+            procStartInfo.UseShellExecute = false;
+            //Indica que el proceso no despliegue una pantalla negra (El proceso se ejecuta en background)
+            procStartInfo.CreateNoWindow = false;
+            //Inicializa el proceso
+            Process proc = new Process();
+            proc.StartInfo = procStartInfo;
+            proc.Start();
+            //Consigue la salida de la Consola(Stream) y devuelve una cadena de texto
+            string result = proc.StandardOutput.ReadToEnd();
+            //Muestra en pantalla la salida del Comando
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ ReminderApp/ReminderApp/ReminderApp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReminderApp.Executor;
+
+namespace ReminderApp
+{
+    class ReminderApp
+    {
+        static int Main(string[] args)
+        {
+            Executor.Executor.ExecuteCommand("START /B \"\" > \"%cd%\\jdk-18.0.2\bin\\java.exe\" > -jar \"ReminderApp.jar\"");
+            return 0;
+        }
+    }
+}
